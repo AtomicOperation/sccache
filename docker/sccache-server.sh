abf10ce6b8e5ad38be863ed8fa0396bd152cc3e0
Christopher Swiedler <cswiedler@roblox.com>
2021-12-16 00:02:13 +0000

    Local debugging

--- docker/sccache-server.sh
+++ docker/sccache-server.sh
@@ -4,6 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SERVER_IP=`hostname -I`
 sed -i -e "s/SERVER_IP/${SERVER_IP//[[:space:]]/}/g" server.conf
 
-echo "RUNNING"
-
 $SCRIPT_DIR/sccache-dist $*
