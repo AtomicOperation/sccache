3bba617da379c1e3f9b23e5b77ce591c5d2aa552
Christopher Swiedler <cswiedler@roblox.com>
2021-12-18 00:04:15 +0000

    Log at info in scripts

--- docker/sccache-server.sh
+++ docker/sccache-server.sh
@@ -29,8 +29,8 @@ if [[ $BUILD_ERR != 0 ]]; then
   exit 1
 fi
 
-SCCACHE_NO_DAEMON=1 RUST_LOG=trace,sccache=trace,hyper=trace,rouille=trace SCCACHE_LOG=trace target/linux/debug/sccache-dist scheduler --config docker/scheduler.conf > scheduler.log 2>&1 & 
-SCCACHE_NO_DAEMON=1 RUST_LOG=trace,sccache=trace,hyper=trace,rouille=trace SCCACHE_LOG=trace target/linux/debug/sccache-dist server --config docker/server.conf > server.log 2>&1  &
+SCCACHE_NO_DAEMON=1 RUST_LOG=warn,sccache=debug,hyper=warn target/linux/debug/sccache-dist scheduler --config docker/scheduler.conf > scheduler.log 2>&1 & 
+SCCACHE_NO_DAEMON=1 RUST_LOG=warn,sccache=debug,hyper=warn target/linux/debug/sccache-dist server --config docker/server.conf > server.log 2>&1  &
 tail -f server.log #scheduler.log
 #read -n 1 -s
 #SCCACHE_NO_DAEMON=1 RUST_LOG=trace,sccache=trace,hyper=trace SCCACHE_LOG=trace target/linux/debug/sccache /usr/bin/c++ -o test.o -c test.cpp
